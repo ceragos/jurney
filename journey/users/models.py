538ebbc240c23b239ec8883e5ec8e7fcf9900471
Jesus Antonio Castañeda Valencia <jesus.a.castaneda.v@gmail.com>
2023-05-28 00:52:05 -0500

    Profiles driver and rider

--- journey/users/models.py
+++ journey/users/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
+from django.contrib.gis.db.models import PointField
 
 
 class User(AbstractUser):
@@ -24,3 +25,19 @@ class User(AbstractUser):
 
         """
         return reverse("users:detail", kwargs={"username": self.username})
+
+
+class Driver(models.Model):
+    user = models.ForeignKey(User, verbose_name="user", on_delete=models.PROTECT, related_name="driver_user")
+    current_location = PointField()
+
+    def __str__(self) -> str:
+        return f"{self.user.first_name} {self.user.last_name}"
+    
+
+class Rider(models.Model):
+    user = models.ForeignKey(User, on_delete=models.PROTECT, related_name="driver_user")
+    tokenized_card = models.CharField(max_length=50, null=True, help_text="data created in the payment api")
+
+    def __str__(self) -> str:
+        return f"{self.user.first_name} {self.user.last_name}"
